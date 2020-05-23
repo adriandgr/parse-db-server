9def6d0867da72bca62ec2ac50e20b52000b53d5
Adrian Diaz <web.art@adriandiaz.ca>
2020-05-22 21:03:57 -0400

    configure heroku

---
+++ build/precache-manifest.9b53019cecccf79aa980b21e21ebdcda.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9540d2767cc2a91bcfa827096cbf3f7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29f664d0f3ae8453f14d",
+    "url": "/static/js/2.a25477d5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a25477d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6709972a1561fd4b932",
+    "url": "/static/js/main.d25ab3cd.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
